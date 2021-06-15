46c03f44f04d9ef34d1a9102d6d003b7d34fe79b
James MacMahon <jwm@operand.ca>
2021-06-15 12:10:06 -0400

    functionize and update

--- .vim/plugins.sh
+++ .vim/plugins.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
+function plugin() {
+    URLBASE="${1}"
+    DIR="${2}"
+
+    if [[ ! -e "${DIR}" ]];
+    then
+        echo "pulling ${DIR}"
+        git clone "${URLBASE}"/"${DIR}"
+    else
+        echo "updating ${DIR}"
+        ( cd "${DIR}" && git pull )
+    fi
+}
+
 mkdir -p pack/auto/start/
 
 cd pack/auto/start/
 
-if [[ ! -e vim-gnupg ]];
-then
-    git clone https://github.com/jamessan/vim-gnupg
-fi
-
-if [[ ! -e nerdtree ]];
-then
-    git clone https://github.com/preservim/nerdtree
-fi
-
-if [[ ! -e rust.vim ]];
-then
-    git clone https://github.com/rust-lang/rust.vim
-fi
-
-if [[ ! -e vim-go ]];
-then
-    git clone https://github.com/fatih/vim-go
-fi
-
-if [[ ! -e vimwiki ]];
-then
-    git clone https://github.com/vimwiki/vimwiki
-fi
+plugin https://github.com/jamessan vim-gnupg
+plugin https://github.com/preservim nerdtree
+plugin https://github.com/rust-lang rust.vim
+plugin https://github.com/fatih vim-go
+plugin https://github.com/vimwiki vimwiki
 
